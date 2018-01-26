3cd3bd6a3ad8fe2fcf2487846b8844ac13aaddbe
John Ashton <john.ashton@ladbrokescoral.com>
2018-01-26 17:18:27 +0000

    Change header in main app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Movie Night!</h1>
         </header>
         <div>
           <input type="text" className="search-filter" onChange={this.handleSearchFilterInput}
