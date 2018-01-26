9f9340fe26ffdf6572c6fee0b0dcaf373ccd2520
John Ashton <john.ashton@ladbrokescoral.com>
2018-01-26 16:21:13 +0000

    refactor movie class componenet

--- front-end/src/components/Movies.js
+++ front-end/src/components/Movies.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
 class Movie extends Component {
-// State passed down from parent componenet to render 
+
+// State passed down from parent App componenet to movie props
+
   render() {
     let baseImgURL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2"
     return (
-        <div>
-          <ul>
+        <div className="movie-list container">
             {this.props.movie.map(movies =>
               <div>
                 <img src={baseImgURL + movies.backdrop_path}/>
-                <li> {movies.original_title} </li>
-                <li>{movies.overview}</li>
+                <h2>Movie Title:</h2>{movies.original_title}
+                <h2>Movie description: </h2>{movies.overview}
               </div>)}
-          </ul>
         </div>
     );
   }
